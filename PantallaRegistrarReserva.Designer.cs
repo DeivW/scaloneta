71656c08dc94fa62bd95aafda9b9d861af833b15
FacundoRago <facundorago98@gmail.com>
2022-08-01 13:38:23 -0300

    Cambios en el front

--- PantallaRegistrarReserva.Designer.cs
+++ PantallaRegistrarReserva.Designer.cs
@@ -40,14 +40,17 @@ namespace Gestion_de_RT
             this.lblCheckRT = new System.Windows.Forms.Label();
             this.btnSalir = new System.Windows.Forms.Button();
             this.dgvCalendario = new System.Windows.Forms.DataGridView();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.dgvCalendario)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // btnNvaReserva
             // 
-            this.btnNvaReserva.Location = new System.Drawing.Point(438, 12);
+            this.btnNvaReserva.Location = new System.Drawing.Point(576, 13);
+            this.btnNvaReserva.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnNvaReserva.Name = "btnNvaReserva";
-            this.btnNvaReserva.Size = new System.Drawing.Size(124, 23);
+            this.btnNvaReserva.Size = new System.Drawing.Size(186, 48);
             this.btnNvaReserva.TabIndex = 0;
             this.btnNvaReserva.Text = "Registrar reserva";
             this.btnNvaReserva.UseVisualStyleBackColor = true;
@@ -57,27 +60,32 @@ namespace Gestion_de_RT
             // 
             this.cmbTiposRT.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cmbTiposRT.FormattingEnabled = true;
-            this.cmbTiposRT.Location = new System.Drawing.Point(190, 55);
+            this.cmbTiposRT.Location = new System.Drawing.Point(433, 93);
+            this.cmbTiposRT.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.cmbTiposRT.Name = "cmbTiposRT";
-            this.cmbTiposRT.Size = new System.Drawing.Size(140, 21);
+            this.cmbTiposRT.Size = new System.Drawing.Size(185, 24);
             this.cmbTiposRT.TabIndex = 1;
             this.cmbTiposRT.Visible = false;
             // 
             // lblTiposRT
             // 
             this.lblTiposRT.AutoSize = true;
-            this.lblTiposRT.Location = new System.Drawing.Point(12, 58);
+            this.lblTiposRT.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTiposRT.ForeColor = System.Drawing.Color.White;
+            this.lblTiposRT.Location = new System.Drawing.Point(30, 94);
+            this.lblTiposRT.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblTiposRT.Name = "lblTiposRT";
-            this.lblTiposRT.Size = new System.Drawing.Size(172, 13);
+            this.lblTiposRT.Size = new System.Drawing.Size(300, 20);
             this.lblTiposRT.TabIndex = 2;
             this.lblTiposRT.Text = "Tipos de Recursos Teconológicos:";
             this.lblTiposRT.Visible = false;
             // 
             // btnSelecTipoRT
             // 
-            this.btnSelecTipoRT.Location = new System.Drawing.Point(336, 55);
+            this.btnSelecTipoRT.Location = new System.Drawing.Point(628, 93);
+            this.btnSelecTipoRT.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnSelecTipoRT.Name = "btnSelecTipoRT";
-            this.btnSelecTipoRT.Size = new System.Drawing.Size(75, 23);
+            this.btnSelecTipoRT.Size = new System.Drawing.Size(100, 28);
             this.btnSelecTipoRT.TabIndex = 3;
             this.btnSelecTipoRT.Text = "Seleccionar";
             this.btnSelecTipoRT.UseVisualStyleBackColor = true;
@@ -89,9 +97,10 @@ namespace Gestion_de_RT
             this.lblCheckTipoRT.AutoSize = true;
             this.lblCheckTipoRT.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblCheckTipoRT.ForeColor = System.Drawing.Color.Lime;
-            this.lblCheckTipoRT.Location = new System.Drawing.Point(417, 52);
+            this.lblCheckTipoRT.Location = new System.Drawing.Point(736, 89);
+            this.lblCheckTipoRT.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblCheckTipoRT.Name = "lblCheckTipoRT";
-            this.lblCheckTipoRT.Size = new System.Drawing.Size(25, 26);
+            this.lblCheckTipoRT.Size = new System.Drawing.Size(30, 31);
             this.lblCheckTipoRT.TabIndex = 4;
             this.lblCheckTipoRT.Text = "✓";
             this.lblCheckTipoRT.Visible = false;
@@ -99,28 +108,33 @@ namespace Gestion_de_RT
             // lblRTs
             // 
             this.lblRTs.AutoSize = true;
-            this.lblRTs.Location = new System.Drawing.Point(66, 92);
+            this.lblRTs.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRTs.ForeColor = System.Drawing.Color.White;
+            this.lblRTs.Location = new System.Drawing.Point(118, 136);
+            this.lblRTs.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblRTs.Name = "lblRTs";
-            this.lblRTs.Size = new System.Drawing.Size(118, 13);
+            this.lblRTs.Size = new System.Drawing.Size(212, 20);
             this.lblRTs.TabIndex = 5;
-            this.lblRTs.Text = "Recursos tecnológicos:";
+            this.lblRTs.Text = "Recursos Tecnológicos:";
             this.lblRTs.Visible = false;
             // 
             // cmbRTs
             // 
             this.cmbRTs.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cmbRTs.FormattingEnabled = true;
-            this.cmbRTs.Location = new System.Drawing.Point(190, 89);
+            this.cmbRTs.Location = new System.Drawing.Point(433, 135);
+            this.cmbRTs.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.cmbRTs.Name = "cmbRTs";
-            this.cmbRTs.Size = new System.Drawing.Size(140, 21);
+            this.cmbRTs.Size = new System.Drawing.Size(185, 24);
             this.cmbRTs.TabIndex = 6;
             this.cmbRTs.Visible = false;
             // 
             // btnSelecRT
             // 
-            this.btnSelecRT.Location = new System.Drawing.Point(336, 87);
+            this.btnSelecRT.Location = new System.Drawing.Point(628, 132);
+            this.btnSelecRT.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnSelecRT.Name = "btnSelecRT";
-            this.btnSelecRT.Size = new System.Drawing.Size(75, 23);
+            this.btnSelecRT.Size = new System.Drawing.Size(100, 28);
             this.btnSelecRT.TabIndex = 7;
             this.btnSelecRT.Text = "Seleccionar";
             this.btnSelecRT.UseVisualStyleBackColor = true;
@@ -132,18 +146,20 @@ namespace Gestion_de_RT
             this.lblCheckRT.AutoSize = true;
             this.lblCheckRT.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblCheckRT.ForeColor = System.Drawing.Color.Lime;
-            this.lblCheckRT.Location = new System.Drawing.Point(417, 88);
+            this.lblCheckRT.Location = new System.Drawing.Point(736, 133);
+            this.lblCheckRT.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblCheckRT.Name = "lblCheckRT";
-            this.lblCheckRT.Size = new System.Drawing.Size(25, 26);
+            this.lblCheckRT.Size = new System.Drawing.Size(30, 31);
             this.lblCheckRT.TabIndex = 8;
             this.lblCheckRT.Text = "✓";
             this.lblCheckRT.Visible = false;
             // 
             // btnSalir
             // 
-            this.btnSalir.Location = new System.Drawing.Point(944, 476);
+            this.btnSalir.Location = new System.Drawing.Point(1236, 572);
+            this.btnSalir.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnSalir.Name = "btnSalir";
-            this.btnSalir.Size = new System.Drawing.Size(75, 23);
+            this.btnSalir.Size = new System.Drawing.Size(123, 42);
             this.btnSalir.TabIndex = 9;
             this.btnSalir.Text = "Salir";
             this.btnSalir.UseVisualStyleBackColor = true;
@@ -154,18 +170,32 @@ namespace Gestion_de_RT
             this.dgvCalendario.AllowUserToAddRows = false;
             this.dgvCalendario.AllowUserToDeleteRows = false;
             this.dgvCalendario.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvCalendario.Location = new System.Drawing.Point(140, 209);
+            this.dgvCalendario.Location = new System.Drawing.Point(175, 190);
+            this.dgvCalendario.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.dgvCalendario.Name = "dgvCalendario";
             this.dgvCalendario.ReadOnly = true;
-            this.dgvCalendario.Size = new System.Drawing.Size(765, 166);
+            this.dgvCalendario.RowHeadersWidth = 51;
+            this.dgvCalendario.Size = new System.Drawing.Size(1020, 235);
             this.dgvCalendario.TabIndex = 10;
             this.dgvCalendario.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvTurnos_CellDoubleClick);
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Gestion_de_RT.Properties.Resources.imagen;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 448);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(309, 166);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 11;
+            this.pictureBox1.TabStop = false;
+            // 
             // PantallaRegistrarReserva
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1031, 511);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(4)))), ((int)(((byte)(154)))), ((int)(((byte)(191)))));
+            this.ClientSize = new System.Drawing.Size(1375, 629);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.dgvCalendario);
             this.Controls.Add(this.btnSalir);
             this.Controls.Add(this.lblCheckRT);
@@ -177,11 +207,13 @@ namespace Gestion_de_RT
             this.Controls.Add(this.lblTiposRT);
             this.Controls.Add(this.cmbTiposRT);
             this.Controls.Add(this.btnNvaReserva);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.MaximizeBox = false;
             this.Name = "PantallaRegistrarReserva";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Registrar reserva de un RT";
             ((System.ComponentModel.ISupportInitialize)(this.dgvCalendario)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -200,6 +232,7 @@ namespace Gestion_de_RT
         private System.Windows.Forms.Label lblCheckRT;
         private System.Windows.Forms.Button btnSalir;
         private System.Windows.Forms.DataGridView dgvCalendario;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
